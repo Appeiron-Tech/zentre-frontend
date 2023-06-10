020e70acac3b68c76e943cb3a6e0985e74d5f2fc
Ernesto <ernesto@appeiron.tech>
2023-06-10 17:39:04 +0200

    TS-1 | Moving dialog to a new component

--- src/views/courses/CoursesView.vue
+++ src/views/courses/CoursesView.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
 import AddSection from './components/AddSection.vue'
+import AddCourseDialog from './components/AddCourseDialog.vue'
 
 const showToggleCourseForm = ref(false)
 
@@ -19,31 +20,7 @@ function toggleCourseForm() {
         </div>
       </div>
 
-      <q-dialog
-        v-model="showToggleCourseForm"
-        transition-show="flip-down"
-        transition-hide="flip-up"
-      >
-        <q-card class="bg-primary text-white">
-          <q-bar>
-            <q-space />
-
-            <q-btn dense flat icon="close" v-close-popup>
-              <q-tooltip class="bg-white text-primary">Close</q-tooltip>
-            </q-btn>
-          </q-bar>
-
-          <q-card-section>
-            <div class="text-h6">Alert</div>
-          </q-card-section>
-
-          <q-card-section class="q-pt-none">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit voluptate
-            voluptas eveniet porro. Rerum blanditiis perferendis totam, ea at omnis vel numquam
-            exercitationem aut, natus minima, porro labore.
-          </q-card-section>
-        </q-card>
-      </q-dialog>
+      <AddCourseDialog v-model="showToggleCourseForm" />
     </main>
   </div>
 </template>
---
+++ src/views/courses/components/AddCourseDialog.vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup>
+import { computed } from 'vue'
+
+const props = defineProps(['modelValue'])
+const emit = defineEmits(['update:modelValue'])
+
+const value = computed({
+  get() {
+    return props.modelValue
+  },
+  set(value) {
+    emit('update:modelValue', value)
+  },
+})
+</script>
+
+<template>
+  <q-dialog v-model="value" transition-show="flip-down" transition-hide="flip-up">
+    <q-card class="bg-primary text-white">
+      <q-bar>
+        <q-space />
+
+        <q-btn dense flat icon="close" v-close-popup>
+          <q-tooltip class="bg-white text-primary">Close</q-tooltip>
+        </q-btn>
+      </q-bar>
+
+      <q-card-section>
+        <div class="text-h6">Alert</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit voluptate
+        voluptas eveniet porro. Rerum blanditiis perferendis totam, ea at omnis vel numquam
+        exercitationem aut, natus minima, porro labore.
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+</template>
