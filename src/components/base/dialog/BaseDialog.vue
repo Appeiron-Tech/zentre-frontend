835f307477d6eec776997548a13f0ae98780bba4
Ernesto <ernesto@appeiron.tech>
2023-06-14 00:17:28 +0200

    Using dialog and stepper

---
+++ src/components/base/dialog/BaseDialog.vue
@@ -0,0 +1,8 @@
+<!-- <script lang="ts" setup>
+
+</script>
+
+<template>
+  <q-dialog v-model="value" transition-show="flip-down" transition-hide="flip-up">
+  </q-dialog>
+</template> -->
--- src/views/courses/components/AddCourseDialog.vue
+++ src/views/courses/components/AddCourseDialog.vue
@@ -1,8 +1,10 @@
 <script lang="ts" setup>
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 
 const props = defineProps(['modelValue'])
 const emit = defineEmits(['update:modelValue'])
+
+const step = ref(1)
 
 const value = computed({
   get() {
@@ -16,24 +18,53 @@ const value = computed({
 
 <template>
   <q-dialog v-model="value" transition-show="flip-down" transition-hide="flip-up">
-    <q-card class="bg-primary text-white">
-      <q-bar>
+    <div style="max-width: 700px">
+      <q-bar class="bg-primary text-white">
         <q-space />
-
         <q-btn dense flat icon="close" v-close-popup>
-          <q-tooltip class="bg-white text-primary">Close</q-tooltip>
+          <q-tooltip class="bg-primary text-white">Close</q-tooltip>
         </q-btn>
       </q-bar>
+      <q-stepper v-model="step" ref="stepper" color="primary" animated>
+        <q-step :name="1" title="Title" icon="settings" :done="step > 1">
+          For each ad campaign that you create, you can control how much you're willing to spend on
+          clicks and conversions, which networks and geographical locations you want your ads to
+          show on, and more.
+        </q-step>
 
-      <q-card-section>
-        <div class="text-h6">Alert</div>
-      </q-card-section>
+        <q-step
+          :name="2"
+          title="Description"
+          caption="Optional"
+          icon="create_new_folder"
+          :done="step > 2"
+        >
+          An ad group contains one or more ads which target a shared set of keywords.
+        </q-step>
 
-      <q-card-section class="q-pt-none">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit voluptate
-        voluptas eveniet porro. Rerum blanditiis perferendis totam, ea at omnis vel numquam
-        exercitationem aut, natus minima, porro labore.
-      </q-card-section>
-    </q-card>
+        <q-step :name="3" title="Public" icon="add_comment">
+          Try out different ad text to see what brings in the most customers, and learn how to
+          enhance your ads using features like ad extensions. If you run into any problems with your
+        </q-step>
+
+        <template v-slot:navigation>
+          <q-stepper-navigation>
+            <q-btn
+              @click=";($refs.stepper as any).next()"
+              color="primary"
+              :label="step === 4 ? 'Finish' : 'Continue'"
+            />
+            <q-btn
+              v-if="step > 1"
+              flat
+              color="primary"
+              @click=";($refs.stepper as any).previous()"
+              label="Back"
+              class="q-ml-sm"
+            />
+          </q-stepper-navigation>
+        </template>
+      </q-stepper>
+    </div>
   </q-dialog>
 </template>
