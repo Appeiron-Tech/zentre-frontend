dcf4ddff9b0003fc5e513fccb3ef5de67ee5bb3f
Ernesto <ernesto@appeiron.tech>
2023-06-21 00:51:14 +0200

    First try of course view

--- src/views/courses/CourseView.vue
+++ src/views/courses/CourseView.vue
@@ -1,9 +1,47 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+const title = ref('')
+const description = ref('')
+</script>
+
 <template>
   <div>
     <main>
-      <div class="col-9 q-pa-md">
+      <div class="col-8 q-pa-md">
         <div class="q-gutter-y-md">
-          <h1>Course View {{ $route.params.id }}</h1>
+          <q-card>
+            <q-card-section class="q-mx-xl">
+              <h2>{{ $t('CourseEdit.pageTitle') }}</h2>
+              <h5>{{ $t('CourseEdit.pageDescription') }}</h5>
+            </q-card-section>
+            <q-separator inset />
+            <q-card-section class="q-mt-lg q-mx-xl">
+              <div>
+                <q-input
+                  outlined
+                  v-model="title"
+                  :label="$t('CourseEdit.titleLabel')"
+                  :hint="$t('CourseEdit.titleHint')"
+                  :rules="[
+                    (val) =>
+                      (val && val.length > 0 && val.length <= 60) || $t('CreateCourses.titleError'),
+                  ]"
+                />
+              </div>
+              <div class="q-mt-lg">
+                <q-input
+                  outlined
+                  v-model="description"
+                  :label="$t('CourseEdit.descriptionLabel')"
+                  :hint="$t('CourseEdit.descriptionHint')"
+                  :rules="[
+                    (val) =>
+                      (val && val.length > 0 && val.length <= 60) || $t('CreateCourses.descError'),
+                  ]"
+                />
+              </div>
+            </q-card-section>
+          </q-card>
         </div>
       </div>
     </main>
