8bc4f36660efcb0e6302409ab49ba208563ca164
Ernesto <ernesto@appeiron.tech>
2023-09-21 23:48:07 +0200

    Fix bug validating min length on course input

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,7 +1,7 @@
 export const maxLength = (val: string, length: number) =>
   val && val.length > 0 && val.length <= length
 
-export const minLength = (val: string, length: number) => val && val.length <= length
+export const minLength = (val: string, length: number) => val && val.length >= length
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function isObjectEmpty(object: any): boolean {
